5cf67d9052852d691ff3631d3a8749ca93e05763
Remi Germain <rgermain@z1r1p3.le-101.fr>
2019-09-30 17:35:19 +0200

    refont divers

--- srcs/regex_enclosed.c
+++ srcs/regex_enclosed.c
@@ -6,37 +6,11 @@
 /*   By: rgermain <rgermain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 15:48:43 by rgermain          #+#    #+#             */
-/*   Updated: 2019/09/30 17:15:39 by rgermain         ###   ########.fr       */
+/*   Updated: 2019/09/30 17:35:12 by rgermain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "regex.h"
-
-t_bool		regex_enclose_do(t_regex *st, t_reg_quan *quantifier, char *s1, char *reg)
-{
-	int i;
-
-	i = 0;
-	while ((quantifier->isset & QUAN_LAZY) && i <= quantifier->match)
-	{
-		if (verif_quantifier(quantifier, i))
-		{
-			if (regex_parse(st, s1 + i, reg))
-				return (TRUE);
-		}
-		i++;
-	}
-	while (!(quantifier->isset & QUAN_LAZY) && quantifier->match >= 0)
-	{
-		if (verif_quantifier(quantifier, quantifier->match))
-		{
-			if (regex_parse(st, s1 + quantifier->match, reg))
-				return (TRUE);
-		}
-		quantifier->match--;
-	}
-	return (FALSE);
-}
 
 t_bool		regex_enclose_parse(t_regex *st, t_reg_quan *quantifier, char *s1, char *reg, int len, int index)
 {
@@ -77,10 +51,8 @@ t_bool  regex_enclosed(t_regex *st, char *s1, char *reg)
     int         len;
 
     ft_bzero(&quantifier, sizeof(t_reg_quan));
-	len = regex_span_enclose(st, reg) + 1;
+	len = regex_span_enclose(st, reg);
 	if (is_quantifier(st, reg + len))    
         len += regex_get_quantifier(&quantifier, reg + len);
     return (regex_enclose_parse(st, &quantifier, s1, reg, len, 0));
-    //return (regex_enclose_do(st, &quantifier, s1, reg + len));
-
 }
